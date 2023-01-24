8d6d52a3d0af91802636c810cabe49ee9ec30f7b
Segun Osiki <osiki.segun@gmail.com>
2023-01-24 20:39:54 +0100

    add files

--- test.js
+++ test.js
@@ -1,1 +1,9 @@
-alert("hello world");
+function setTheme() {
+    const root = document.documentElement;
+    const newTheme = root.className === 'dark' ? 'light' : 'dark';
+    root.className = newTheme;
+    
+    document.querySelector('.theme-name').textContent = newTheme;
+  }
+  
+  document.querySelector('.theme-toggle').addEventListener('click', setTheme)
