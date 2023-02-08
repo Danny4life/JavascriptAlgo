97dc5d18a40987440abcac0413c0e92149d0f7a9
Segun Osiki <osiki.segun@gmail.com>
2023-02-08 16:41:23 +0100

    solved kata 8

---
+++ Unusualfive.js
@@ -0,0 +1,27 @@
+// Terminal Game - Create Hero Prototype
+// // In this first kata in the series, you need to define a Hero 
+// Write a function that always returns 5
+
+
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 
+// 0123456789*+-/
+
+// Good luck :)
+
+
+// describe("unusualFive", function(){
+//   it("should return 5", function(){
+//     assert.strictEqual(unusualFive(), 5);
+//   });
+// });
+
+
+function unusualFive() {
+
+    let res = "fucku";
+    
+  return res.length;
+}
+
+console.log(unusualFive())
--- test.js
+++ test.js
@@ -1,9 +1,38 @@
-function setTheme() {
-    const root = document.documentElement;
-    const newTheme = root.className === 'dark' ? 'light' : 'dark';
-    root.className = newTheme;
+// function setTheme() {
+//     const root = document.documentElement;
+//     const newTheme = root.className === 'dark' ? 'light' : 'dark';
+//     root.className = newTheme;
     
-    document.querySelector('.theme-name').textContent = newTheme;
-  }
+//     document.querySelector('.theme-name').textContent = newTheme;
+//   }
   
-  document.querySelector('.theme-toggle').addEventListener('click', setTheme)
+//   document.querySelector('.theme-toggle').addEventListener('click', setTheme)
+
+import { v4 as uuidv4 } from 'uuid';
+
+
+const idOne = uuidv4();
+const idTwo = uuidv4();
+
+
+let users = {
+  [idOne]: {
+    id: idOne,
+    firstName: 'Robin',
+    lastName: 'Wieruch',
+    isDeveloper: true,
+  },
+  [idTwo]: {
+    id: idTwo,
+    firstName: 'Dave',
+    lastName: 'Davddis',
+    isDeveloper: false,
+  },
+};
+
+
+const getUsers = () => Object.values(users);
+
+const result = getUsers;
+
+console.log(result);
