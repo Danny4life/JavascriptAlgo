9c3789fe321bbb4fd92cdb98c620482f32f2f968
Segun Osiki <osiki.segun@gmail.com>
2023-03-05 18:19:00 +0100

    solved kata 8

--- test.js
+++ test.js
@@ -5,34 +5,8 @@
     
 //     document.querySelector('.theme-name').textContent = newTheme;
 //   }
-  
-//   document.querySelector('.theme-toggle').addEventListener('click', setTheme)
+async function myFunc(){
+  console.log("hello");
+}
 
-import { v4 as uuidv4 } from 'uuid';
-
-
-const idOne = uuidv4();
-const idTwo = uuidv4();
-
-
-let users = {
-  [idOne]: {
-    id: idOne,
-    firstName: 'Robin',
-    lastName: 'Wieruch',
-    isDeveloper: true,
-  },
-  [idTwo]: {
-    id: idTwo,
-    firstName: 'Dave',
-    lastName: 'Davddis',
-    isDeveloper: false,
-  },
-};
-
-
-const getUsers = () => Object.values(users);
-
-const result = getUsers;
-
-console.log(result);
+console.log(myFunc())
